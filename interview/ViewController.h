e66272564651133a065d29af522a2d1d4f4dd09c
Zacharysp <sp151018@gmail.com>
2015-03-21 08:33:35 -0700

    Initial Commit

---
+++ interview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  interview
+//
+//  Created by Dongjie Zhang on 3/21/15.
+//  Copyright (c) 2015 CCCBTICC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ interview/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  interview
+//
+//  Created by Dongjie Zhang on 3/21/15.
+//  Copyright (c) 2015 CCCBTICC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
